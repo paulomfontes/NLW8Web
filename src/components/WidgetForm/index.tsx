02b8d9dc0b59da6432f814cbe7a7baff99d37395
Paulo Fontes <paulomfontes@gmail.com>
2022-05-04 21:52:20 -0300

    Aula 2 parte 2

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -37,6 +37,10 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm(){
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function  handleRestartFeedback(){
+    setFeedbackType(null)
+  }
+
   return(
     <div className="gb-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
            
@@ -44,7 +48,9 @@ export function WidgetForm(){
       <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
 
       ) : ( 
-        <FeedbackContentStep feedbackType={feedbackType}/>
+        <FeedbackContentStep 
+        feedbackType={feedbackType}
+        onFeedbackRestartRequested={handleRestartFeedback}/>
       )}
 
       <footer className="text-xs text-neutral-400">
